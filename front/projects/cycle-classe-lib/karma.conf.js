c9a8c5e7801319286bb92da03f8139264ed1fce1
JoBelix <jo.belix@outlook.fr>
2021-12-20 15:57:12 +0100

    fix: correction sur chemin code coverage

--- front/projects/cycle-classe-lib/karma.conf.js
+++ front/projects/cycle-classe-lib/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/daily-child-app'),
+      dir: require('path').join(__dirname, '../../coverage/cycle-classe-lib'),
       subdir: '.',
       reporters: [
         { type: 'lcov' },
--- front/projects/cycle1-objectif-lib/karma.conf.js
+++ front/projects/cycle1-objectif-lib/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/daily-child-app'),
+      dir: require('path').join(__dirname, '../../coverage/cycle1-objectif-lib'),
       subdir: '.',
       reporters: [
         { type: 'lcov' },
