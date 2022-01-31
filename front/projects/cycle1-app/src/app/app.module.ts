5d5d34030b812d371fa1461b940d9e2982eb25e3
jo-belix <95655888+jo-belix@users.noreply.github.com>
2022-01-31 09:33:54 +0100

    feat : pwa + localisation

--- front/projects/cycle1-app/src/app/app.module.ts
+++ front/projects/cycle1-app/src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { SharedLibModule } from '@shared-lib';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -16,9 +19,15 @@ import { CoreModule } from './core/core.module';
     SharedLibModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
