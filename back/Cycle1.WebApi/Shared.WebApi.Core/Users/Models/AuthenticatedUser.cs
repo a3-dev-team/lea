c69a5a7e9ef68ecb1d599f4bc04b088c207b50e3
BoiteAChips <bertrand.dolet@gmail.com>
2022-02-04 18:22:10 +0100

    fix: Ajout de l'email sur AutehnticatedUser

--- back/Cycle1.WebApi/Shared.WebApi.Core/Users/Models/AuthenticatedUser.cs
+++ back/Cycle1.WebApi/Shared.WebApi.Core/Users/Models/AuthenticatedUser.cs
@@ -6,6 +6,8 @@
 
         public string LastName { get; init; } = string.Empty;
 
+        public string Email { get; set; }
+
         public string Role { get; init; } = string.Empty;
 
         public string Token { get; init; } = string.Empty;
